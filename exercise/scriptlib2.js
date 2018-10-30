14a5dbd9a2001d923b21c9333f112d3db636f41d
YvonneXin47 <43102331+YvonneXin47@users.noreply.github.com>
2018-10-30 18:34:51 +0800

    change js

--- exercise/scriptlib2.js
+++ exercise/scriptlib2.js
@@ -12,28 +12,28 @@ $(document).ready(function(){
     $("button#get_data").click(function() {
         var items = [];
         var i = 0;
-        var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appck1ixyjJBoAosx/%E4%B8%8A%E6%B5%B7?api_key=keyx6ScOw6mCv5Vc4";
         var dataSet = [];
         $.getJSON(airtable_read_endpoint, function(result) {
                $.each(result.records, function(key,value) {
                    items = [];
-                       items.push(value.fields.Name);
-                       items.push(value.fields.Completed);
-                       items.push(value.fields.Time_Estimate);
-                       items.push(value.fields.converted);
+                       items.push(value.fields.poi);
+                       items.push(value.fields.intro);
+                       items.push(value.fields.location);
+                       items.push(value.fields.time);
                        dataSet.push(items);
                 }); // end .each
              $('#example').DataTable( {
                  data: dataSet,
                  retrieve: true,
                  columns: [
-                     { title: "Name",
+                     { title: "poi",
                        defaultContent:""},
-                     { title: "Completed",
+                     { title: "intro",
                        defaultContent:"" },
-                     { title: "Time Estimated",
+                     { title: "location",
                        defaultContent:""},
-                     { title: "Converted",
+                     { title: "time",
                        defaultContent:""},
                  ]
              } );
